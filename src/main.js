dd23a852e297d4c240169d834942a7da07d61c5d
ygyoo <ygyoo@mtdata.co.kr>
2024-04-09 14:41:07 +0900

    vue router 추가

--- src/main.js
+++ src/main.js
@@ -6,15 +6,30 @@
 
 // Plugins
 import { registerPlugins } from '@/plugins'
+import { createMemoryHistory, createRouter } from "vue-router";
+import { createApp } from 'vue'
 
 // Components
 import App from './App.vue'
+import CompanyList from './views/company/CompanyList.vue'
+
+
+// set router
+const routes = [
+  { path: '/'                       , component: App },
+  { path: '/company/companyList'    , component: CompanyList},
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
 
 // Composables
-import { createApp } from 'vue'
-
 const app = createApp(App)
 
 registerPlugins(app)
 
-app.mount('#app')
+app
+  .use(router)
+  .mount('#app')
---
+++ src/views/company/CompanyList.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log('test');
+    alert('test');
+  },
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
