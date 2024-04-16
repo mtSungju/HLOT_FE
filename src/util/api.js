65335f0a8be591d3578c67c305717ce7ff6b2f53
ygyoo <ygyoo@mtdata.co.kr>
2024-04-16 16:55:35 +0900

    샘플데이터 - 유영국

--- src/util/api.js
+++ src/util/api.js
@@ -1,4 +1,6 @@
 import company from "@/sampleData/company.json"
+import estimate from "@/sampleData/estimate.json"
+import project from "@/sampleData/project.json"
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const BE_PORT = import.meta.env.VITE_BE_PORT;
@@ -25,8 +27,15 @@ export default {
 
   companySampleData: () => {
     return company.companyData;
-  }
+  },
 
+  estimateSampleData: () => {
+    return estimate.estimateData;
+  },
+
+  projectSampleData: () => {
+    return project.proejctData;
+  }
 }
 
 
