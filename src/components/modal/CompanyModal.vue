2c6cdb74577f2b248171e5bd562c2f5b615d13bc
ygyoo <ygyoo@mtdata.co.kr>
2024-04-17 15:10:05 +0900

    modal css - 유영국

--- src/components/modal/CompanyModal.vue
+++ src/components/modal/CompanyModal.vue
@@ -5,7 +5,39 @@
     </div>
 
     <div class="modal-content">
-      Test입니다~
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="company.companyName"
+              label="업체명"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="company.businessRegistNumb"
+              label="사업자번호"
+            />
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="company.companyTel"
+              label="전화번호"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-textarea
+              filled
+              label="비고"
+              v-model="company.remark"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
     </div>
   </ModalLayout>
 </template>
@@ -16,7 +48,17 @@ import ModalLayout from "@/layouts/ModalLayout.vue";
 
 <script>
 export default {
-  name: "CompanyModal"
+  name: "CompanyModal",
+  data(){
+    return {
+      company: {
+        companyName: '',        // 업체명
+        businessRegistNumb: '', // 사업자번호
+        companyTel: '',         // 업체번호
+        remark: '',             // 비고
+      }
+    }
+  }
 }
 </script>
 
