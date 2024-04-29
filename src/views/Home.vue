9b065843c58d5aa174083e8d7d134e0d78c1315c
choisungju <tjdwn0320@naver.com>
2024-04-29 10:27:27 +0900

    HLOT_FE COMMIT

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,10 +17,10 @@
           </v-sheet>
           <v-container>
             <v-textarea label="메모장" variant="outlined" rows="10"></v-textarea>
+            <div id="map" style="width: 500px; height: 400px;"></div> 
           </v-container>
         </v-col>
       </v-row>
-
     </v-container>
   </v-main>
 
@@ -39,5 +39,33 @@ export default {
       datePicker: new Date(),
     }
   },
+  
+  mounted() {
+    
+   const script = document.createElement('script');
+   script.type = 'text/javascript';
+   script.src = "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=2c59ccb1e34c6a67775ee5b48ed9042b";
+   script.addEventListener('load',()=>{
+    kakao.maps.load(()=>{
+      this.initMap()
+    })
+   })
+   document.head.appendChild(script);
+  },
+
+  methods :{
+    initMap(){
+      const container = document.getElementById('map');
+
+      const options = {
+        center: new kakao.maps.LatLng(37.403940, 127.099853),
+        level: 3
+      };
+      
+      this.map = new kakao.maps.Map(container,options)
+    }
+  }
+
 }
+
 </script>
